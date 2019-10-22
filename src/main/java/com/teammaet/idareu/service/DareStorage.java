163855eaa6dc1c7ee0af9098f2a878b64a5282b4
Joó Norbert <joonorbi12@gmail.com>
2019-10-22 09:45:33 +0200

    Renamed DareService to DareStorage.Add sentDares and receivedDares list to DaresStorage.Added add and delete functionality.

---
+++ src/main/java/com/teammaet/idareu/service/DareStorage.java
@@ -0,0 +1,38 @@
+package com.teammaet.idareu.service;
+
+import com.teammaet.idareu.model.Dare;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DareStorage {
+    private List<Dare> sentDares = new ArrayList<>();
+    private List<Dare> receivedDares = new ArrayList<>();
+
+    public void createDare(String title, String dare, String bet, Date deadline){
+        Dare dare1 = new Dare(title,dare,bet,deadline);
+    }
+    public void add(List<Dare> dareList,Dare dare){
+        dareList.add(dare);
+    }
+    public Dare getDareById(int id,List<Dare> dareList){
+        for(Dare dare : dareList){
+            if(id == dare.getId()){
+                return dare;
+            }
+        }
+        return null;
+    }
+    public void delete (List<Dare> dareList,Dare dare) {
+        dareList.remove(dare);
+    }
+
+    public List<Dare> getSentDares() {
+        return sentDares;
+    }
+
+    public List<Dare> getReceivedDares() {
+        return receivedDares;
+    }
+}
