c53602edb2bbfa22c8cc7ce80a3705632e99d61a
Joó Norbert <joonorbi12@gmail.com>
2019-10-23 16:00:47 +0200

    Tested UserStorage methods

---
+++ src/test/java/com/teammaet/idareu/service/UserStorageTest.java
@@ -0,0 +1,49 @@
+package com.teammaet.idareu.service;
+
+import com.teammaet.idareu.model.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserStorageTest {
+    UserStorage userStorage=new UserStorage();
+
+    @Test
+    void register() {
+        User user = new User("ADAM","ADAM@gamil.com","asd");
+        userStorage.register(user);
+        assert(!userStorage.getUsers().isEmpty());
+    }
+
+    @Test
+    void deleteUser() {
+        User user = new User("ADAM","ADAM@gamil.com","asd");
+        userStorage.register(user);
+        userStorage.deleteUser(user);
+        assert(userStorage.getUsers().isEmpty());
+    }
+
+    @Test
+    void getUserById() {
+        User user = new User("ADAM","ADAM@gamil.com","asd");
+        int id = user.getId();
+        userStorage.register(user);
+        assertEquals(id,userStorage.getUserById(id).getId());
+    }
+
+    @Test
+    void update() {
+        User user = new User("ADAM","ADAM@gamil.com","asd");
+        String name = "Jump";
+        String email = "Well";
+        String password = "Bet";
+        userStorage.register(user);
+        userStorage.update(user,name,email,password);
+        assertEquals(name,user.getName());
+        assertEquals(email,user.getEmail());
+        assertEquals(password,user.getPassword());
+    }
+}
