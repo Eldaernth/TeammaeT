3ddbe1b03b0bbc011ed36401b502d0edba700059
Tamas Floch <tamasfloch95@gmail.com>
2019-10-23 14:32:38 +0200

    removed User Creator.

--- src/main/java/com/teammaet/idareu/controller/DareController.java
+++ src/main/java/com/teammaet/idareu/controller/DareController.java
@@ -2,42 +2,48 @@ package com.teammaet.idareu.controller;
 
 
 import com.teammaet.idareu.model.Dare;
+import com.teammaet.idareu.model.Friend;
 import com.teammaet.idareu.model.User;
-import com.teammaet.idareu.service.UserCreator;
 import com.teammaet.idareu.service.UserStorage;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/user/{id}/dares")
+@RequestMapping("/user/{userId}/dare")
 public class DareController {
 
-    private UserCreator userCreator;
     private UserStorage userStorage;
 
-    public DareController(UserCreator userCreator, UserStorage userStorage) {
-        this.userCreator = userCreator;
+    public DareController(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
 
     @GetMapping("/received")
-    public List<Dare> getReceivedDares(@PathVariable("id") int id) throws Exception {
-        User user = userStorage.getUserById(id);
+    public List<Dare> getReceivedDares(@PathVariable("userId") int userId) throws Exception {
+        User user = userStorage.getUserById(userId);
         return user.getDareStorage().getReceivedDares();
     }
 
     @GetMapping("/sent")
-    public List<Dare> getSentDares(@PathVariable("id") int id) throws Exception {
-        User user = userStorage.getUserById(id);
+    public List<Dare> getSentDares(@PathVariable("userId") int userId) throws Exception {
+        User user = userStorage.getUserById(userId);
         return user.getDareStorage().getSentDares();
     }
 
     @GetMapping("/all")
-    public List<Dare> getDares(@PathVariable("id") int id) throws Exception {
-        User user = userStorage.getUserById(id);
+    public List<Dare> getDares(@PathVariable("userId") int userId) throws Exception {
+        User user = userStorage.getUserById(userId);
         return user.getDareStorage().getAllDare();
     }
 
-
+    @PostMapping("/send")
+    public Dare createDare(@PathVariable("userId") int userId,
+                           @RequestBody Dare dare,
+                           @RequestBody Set<Friend> friendList)  throws Exception {
+        User user = userStorage.getUserById(userId);
+        user.send(dare, friendList);
+        return dare;
+    }
 }
--- src/test/java/com/teammaet/idareu/model/UserTest.java
+++ src/test/java/com/teammaet/idareu/model/UserTest.java
@@ -1,26 +1,21 @@
 package com.teammaet.idareu.model;
 
-import com.teammaet.idareu.service.UserCreator;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 class UserTest {
 
-    @Autowired
-    UserCreator userCreator;
-
     @Test
     void addFriend() {
-        User user = userCreator.createUser("name 1", "email 1", "pass 1");
-        User friend = userCreator.createUser("name 2", "email 2", "pass 2");
+        User user = new User("name 1", "email 1", "pass 1");
+        User friend = new User("name 2", "email 2", "pass 2");
         user.addFriend(friend);
         assert(!user.getFriendList().isEmpty());
     }
 
     @Test
     void deleteFriend() {
-        User user = userCreator.createUser("name 1", "email 1", "pass 1");
-        User friend = userCreator.createUser("name 2", "email 2", "pass 2");
+        User user = new User("name 1", "email 1", "pass 1");
+        User friend = new User("name 2", "email 2", "pass 2");
         user.addFriend(friend);
         user.deleteFriend(friend);
         assert(user.getFriendList().isEmpty());
