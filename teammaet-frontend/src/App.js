f411898912065ee08443fa84dfea8fb984ecc630
Tamas Floch <tamasfloch95@gmail.com>
2019-10-24 17:56:59 +0200

    Created FriendList Route in App

--- teammaet-frontend/src/App.js
+++ teammaet-frontend/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import FriendList from './FriendList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import UsersPage from "./UsersPage"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-    <div className="App">
-        <Switch>
-            <Route path={"/users"} component={UsersPage}/>
-            <Route path={"/users"} render={(props => <FriendList{...props} id=0)}/>
-        </Switch>
-    </div>
-  </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Route path="/users/:id/friend" render={(props) => <FriendList {...props} />}/> 
+            </div>
+        </Router>
+    );
 }
 
 export default App;
