98caf7d22279ebf73bd502f55e78e3f00ea95982
Joó Norbert <joonorbi12@gmail.com>
2019-11-11 19:37:51 +0100

    added table to SentDares

--- teammaet-frontend/src/component/dare/SentDares.js
+++ teammaet-frontend/src/component/dare/SentDares.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import Axios from 'axios';
+import {Button, Table} from "react-bootstrap";
 
 function SentDares(props) {
     const[dares,setDares] = useState([]);
@@ -9,9 +10,23 @@ function SentDares(props) {
         });
     return (
         <div>
-            {dares.map((row) => 
-                <div key={row.id}> {row.title}</div>
+            <Table>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Massage</th>
+                </tr>
+                </thead>
+                <tbody>
+                {dares.map((row) =>
+                    <tr>
+                        <td > {row.title}</td>
+                        <td> {row.dare}</td>
+                        <td><Button value={row.id} variant="outline-danger" onClick={methods.deleteDare}>X</Button></td>
+                    </tr>
                 )}
+                </tbody>
+            </Table>
         </div>
     )
 }
