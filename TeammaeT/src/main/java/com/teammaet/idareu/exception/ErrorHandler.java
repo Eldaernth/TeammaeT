a9e3cd17699acdec669ca09330f91fece7509858
Tamas Floch <tamasfloch95@gmail.com>
2019-11-24 12:15:33 +0100

    removed AuthenticationException from ErrorHandler

--- TeammaeT/src/main/java/com/teammaet/idareu/exception/ErrorHandler.java
+++ TeammaeT/src/main/java/com/teammaet/idareu/exception/ErrorHandler.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.naming.AuthenticationException;
-
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
